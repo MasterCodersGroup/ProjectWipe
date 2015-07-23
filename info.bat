dd8bf8f0c5050dfbc7c7c25df1bcb24b3e4e19fa
prohippocoder <prohippocoder@gmail.com>
2015-07-23 16:09:20 +0800

    Create info.bat

---
+++ info.bat
@@ -0,0 +1,12 @@
+@echo off
+title ProjectWipe Info
+echo.
+echo.
+echo Project
+echo  Wipe
+echo.
+echo.
+echo Copyright MasterCodersGroup 2014 2015
+echo Copyright SuperSeed 2012 2013 2014 2015
+echo Copyright SuperCoad 2013 2014 2015
+echo MasterCodersGroup, SuperSeed, SuperCoad All Rights Reserved
